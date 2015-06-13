36b2d470a5b7e00b9a0224837706bb3e3263429b
Justin Lucas <Lukeas14@gmail.com>
2015-06-13 16:17:22 -0700

    websocket update

--- webapp/websocketExample.js
+++ webapp/websocketExample.js
@@ -1,7 +1,10 @@
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({ port: 3001 });
 
+console.log("Websocket server opened at port: 3001");
+
 wss.on('connection', function connection(ws) {
+	console.log("Connection received on websocket server.");
 	ws.on('message', function incoming(message) {
 		console.log('received: %s', message);
 	});
