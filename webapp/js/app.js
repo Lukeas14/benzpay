cf218cb7fafafbc62667706ff7eb9e7f13016138
Justin Lucas <Lukeas14@gmail.com>
2015-06-13 21:08:42 -0700

    Connecting to comand

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -29,6 +29,48 @@ angular.module('benzpay', ['ngRoute'])
 	})
 
 	.controller('PayController', function($scope, $routeParams) {
+		console.log('set up map');
+		nokia.Settings.set("app_id", "evalLunne37Ciwejfare7");
+		nokia.Settings.set("app_code", "RrEcE54Hc6U2VGp70LqoQQ");
+		var map = new nokia.maps.map.Display(
+			document.getElementById("map-container"), {
+				components: [
+					// Behavior collection
+					new nokia.maps.map.component.Behavior(),
+					new nokia.maps.map.component.Overview(),
+				],
+				// Zoom level for the map
+				zoomLevel: 17,
+				// Map center coordinates
+				center: [37.4121407222, -122.204705778],
+				baseMapType: nokia.maps.map.Display.SMARTMAP
+			}
+		);
+		map.addListener("displayready", function() {
+			// Place code implementing the functionality of app
+			// here, for example to manipulate the map view.
+			window.WebSocket = window.WebSocket || window.MozWebSocket;
+			var connection = new WebSocket('ws://127.0.0.1:3001');
+			connection.onopen = function () {
+				console.log('websocket open');
+			};
+			connection.onerror = function (error) {
+				console.log('websocket error', error);
+			};
+			connection.onmessage = function (message) {
+				var vehicleData = JSON.parse(message.data);
+
+				if(vehicleData.command){
+					console.log('command', vehicleData.command);
+				}
+				else {
+					console.log(vehicleData);
+					map.setCenter([vehicleData.GPS_Latitude, vehicleData.GPS_Longitude]);
+				}
+			};
+		});
+
+
 	})
 
 	.controller('ParkingController', function($scope, $routeParams) {
--- webapp/server.js
+++ webapp/server.js
@@ -1,5 +1,16 @@
 var express = require('express');
 var app = express();
+var request = require('request');
+var WebSocketServer = require('ws').Server;
+var events = require("events");
+var EventEmitter = require("events").EventEmitter;
+var ee = new EventEmitter();
+var serialport = require( "serialport" );
+var SerialPort = serialport.SerialPort;
+
+var vehicleUri = "http://172.31.99.4/vehicle";
+var vehicleData = null;
+var vehicleDataInterval = null;
 
 app.use('/public', express.static('public'));
 
@@ -15,3 +26,67 @@ var server = app.listen(3000, function () {
 	console.log('Example app listening at http://%s:%s', host, port);
 
 });
+
+
+setInterval(function(){
+	request(vehicleUri, function(err, res, body){
+		try {
+			vehicleData = JSON.parse(body);
+		} catch(e){
+			console.log("Bad JSON");
+		}
+		//console.log(body);
+	});
+}, 500);
+
+
+var wss = new WebSocketServer({ port: 3001 });
+console.log("Websocket server opened at port: 3001");
+wss.on('connection', function connection(ws) {
+	console.log("Connection received on websocket server.");
+
+	ws.on('message', function incoming(message) {
+		console.log('received: %s', message);
+	});
+
+	//Send controls
+	var sp = new SerialPort("/dev/tty.usbserial-FTGQJ7IM", {
+		parser: serialport.parsers.readline( "\n" ),
+		baudrate: 115200,
+		databits: 8
+	});
+	sp.on('open', function () {
+		console.log('command connect opened.');
+		sp.on('data', function ( data ) {
+			console.log('command message', {'command': data} );
+			var command = {'command': data};
+
+			ws.send(JSON.stringify(command));
+		});
+	});
+
+	//Send vehicleData
+	vehicleDataInterval = setInterval(function(){
+		//console.log('wtf', JSON.stringify(vehicleData));
+		try {
+			ws.send(JSON.stringify(vehicleData));
+		} catch(e){
+			console.log('message not read');
+		}
+	}, 500);
+
+
+	//Connection closed
+	ws.on('close', function close(){
+		console.log('CLOSING CONNECTION');
+		clearInterval(vehicleDataInterval);
+	});
+});
+
+
+var browserWs;
+
+var wss = new WebSocketServer({ port: 3002 });
+console.log("Websocket server opened at port: 3002");
+
+
