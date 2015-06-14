94e1fa1c43290d79751985c32b0ef9abafb86ffa
Justin Lucas <Lukeas14@gmail.com>
2015-06-13 23:27:29 -0700

    command works

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -1,5 +1,44 @@
 'use strict';
 
+/*
+var chevronMarker = '<svg version="1.1" id="Layer_1" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns"xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="-440 443 118 123"'+
+	'enable-background="new -440 443 118 123" xml:space="preserve">'+
+	'<title>Chevron Marker</title>'+
+	'<desc>Created with Sketch.</desc>'+
+	'<g id="Page-1" sketch:type="MSPage">'+
+	'<g id="Map-menu" transform="translate(-496.000000, -384.000000)" sketch:type="MSArtboardGroup">'+
+	'<g id="Chevron-Marker" transform="translate(496.000000, 384.000000)" sketch:type="MSLayerGroup">'+
+	'<g id="Fuel-Marker-green" sketch:type="MSShapeGroup">'+
+	'<g id="_x37_7_Essential_Icons_11_">'+
+	'<path id="Message" fill="#2BAA5E" d="M-325.8,443.2h-110.9c-1.8,0-3.3,1.5-3.3,3.3v74c0,1.8,1.5,3.3,3.3,3.3h24.3'+
+	'c0.1,0,0.2,0.1,0.2,0.2v18.4c0,2.8,3.2,4.2,5.2,2.2l21-20.7c0,0,0.1-0.1,0.1-0.1h60c1.8,0,3.3-1.5,3.3-3.3v-74'+
+	'C-322.5,444.6-324,443.2-325.8,443.2L-325.8,443.2z M-328.6,517.4c0,0.1-0.1,0.2-0.2,0.2h-58.2c-0.8,0-1.5,0.3-2.2,0.9'+
+	'l-16.4,16.1c-0.1,0.1-0.3,0-0.3-0.1v-13.6c0-1.8-1.5-3.3-3.3-3.3h-24.3c-0.1,0-0.2-0.1-0.2-0.2v-65.8c0-0.1,0.1-0.2,0.2-0.2'+
+	'l104.7-2c0.1,0,0.2,0.1,0.2,0.2V517.4L-328.6,517.4z"/>'+
+	'</g>'+
+	'<path id="Shape" fill="#2BAA5E" d="M-422.9,527.8h-15.3c-0.9,0-1.6,0.8-1.6,1.7v35.5h18.6v-35.5'+
+	'C-421.3,528.6-422.1,527.8-422.9,527.8L-422.9,527.8z M-423.6,541.1h-13.9v-9.9h13.9V541.1L-423.6,541.1z M-414,565.8'+
+	'c-3.8,0-6.1-2.8-6.3-7.8c-0.1-1.9,0.4-4.1,0.8-6.4c0.4-1.7,0.7-3.5,0.9-5.3c0.2-1.8-0.1-2.4-0.3-2.7c-0.3-0.4-1.3-0.4-2.2-0.4'+
+	'h-0.5v-2.4h0.5c1.3,0,2.8,0,3.8,1.2c0.8,0.9,1,2.4,0.8,4.5c-0.2,1.9-0.6,3.8-0.9,5.6c-0.4,2.1-0.9,4.1-0.8,5.7'+
+	'c0.2,3.6,1.5,5.5,4.1,5.5c2.8,0,4.3-1.9,4.3-5.7c0-3.1-0.9-5.6-1.5-7.2l-0.2-0.6c-0.6-1.8-0.7-3.4-0.8-5.7'+
+	'c-0.1-2.3,0-4.8,0.6-7.3c0.6-2.5,1.8-3.7,3.9-5.7l0.5-0.5l1.5,1.8l-0.5,0.5c-1.9,1.9-2.8,2.7-3.2,4.5c-0.5,2.2-0.5,4.5-0.5,6.6'+
+	'c0.1,2.3,0.1,3.6,0.6,5l0.2,0.6c0.6,1.7,1.7,4.5,1.7,8.1C-407.5,562.7-409.9,565.8-414,565.8L-414,565.8z M-411.1,544.4'+
+	'l-0.1-2.4c0,0,1.7,0,2.7-0.1c0-0.4,0-1.1,0-1.8v-0.4c0-2.9-0.4-3.3-0.9-3.7c-0.2-0.2-0.4-0.3-0.5-0.5l1.6-1.6'+
+	'c0.1,0.1,0.2,0.2,0.3,0.3c0.9,0.9,1.7,1.8,1.7,5.6v0.4c0,1.3,0,2.1-0.1,2.7c-0.2,1-0.8,1.5-1.7,1.5'+
+	'C-408.7,544.4-411.1,544.4-411.1,544.4L-411.1,544.4z"/>'+
+	'<path id="Shape_1_" fill="#1B1B1C" d="M-433.7,451.5l0.2,66h24.3l3.6,17l18.5-17l58.4-0.2v-67.8h-105.1V451.5z"/>'+
+	'</g>'+
+	'<text transform="matrix(1 0 0 1 -422 500)" fill="#FFFFFF" font-family="'ArialMT'" font-size="9">12gal @ Chevron</text>'+
+'<image overflow="visible" enable-background="new    " width="2000" height="2235" id="Chevron-logo" xlink:href="1082CB53D4AC3DD1.png"  sketch:type="MSBitmapLayer" transform="matrix(1.300000e-02 0 0 1.297539e-02 -421 455)">'+
+'</image>'+
+'<text transform="matrix(1 0 0 1 -385 478)" fill="#FFFFFF" font-family="'ArialMT'" font-size="24">$50</text>'+
+'</g>'+
+'</g>'+
+'</g>'+
+'</svg>';
+*/
+
+var chevronMarker = '<svg version="1.1" id="Layer_1" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="-440 443 118 123" enable-background="new -440 443 118 123" xml:space="preserve"> <title>Chevron Marker</title> <desc>Created with Sketch.</desc> <g id="Page-1" sketch:type="MSPage"><g id="Map-menu" transform="translate(-496.000000, -384.000000)" sketch:type="MSArtboardGroup"><g id="Chevron-Marker" transform="translate(496.000000, 384.000000)" sketch:type="MSLayerGroup"><g id="Fuel-Marker-green" sketch:type="MSShapeGroup"><g id="_x37_7_Essential_Icons_11_"><path id="Message" fill="#2BAA5E" d="M-325.8,443.2h-110.9c-1.8,0-3.3,1.5-3.3,3.3v74c0,1.8,1.5,3.3,3.3,3.3h24.3c0.1,0,0.2,0.1,0.2,0.2v18.4c0,2.8,3.2,4.2,5.2,2.2l21-20.7c0,0,0.1-0.1,0.1-0.1h60c1.8,0,3.3-1.5,3.3-3.3v-74C-322.5,444.6-324,443.2-325.8,443.2L-325.8,443.2z M-328.6,517.4c0,0.1-0.1,0.2-0.2,0.2h-58.2c-0.8,0-1.5,0.3-2.2,0.9l-16.4,16.1c-0.1,0.1-0.3,0-0.3-0.1v-13.6c0-1.8-1.5-3.3-3.3-3.3h-24.3c-0.1,0-0.2-0.1-0.2-0.2v-65.8c0-0.1,0.1-0.2,0.2-0.2l104.7-2c0.1,0,0.2,0.1,0.2,0.2V517.4L-328.6,517.4z"/></g><path id="Shape" fill="#2BAA5E" d="M-422.9,527.8h-15.3c-0.9,0-1.6,0.8-1.6,1.7v35.5h18.6v-35.5C-421.3,528.6-422.1,527.8-422.9,527.8L-422.9,527.8z M-423.6,541.1h-13.9v-9.9h13.9V541.1L-423.6,541.1z M-414,565.8c-3.8,0-6.1-2.8-6.3-7.8c-0.1-1.9,0.4-4.1,0.8-6.4c0.4-1.7,0.7-3.5,0.9-5.3c0.2-1.8-0.1-2.4-0.3-2.7c-0.3-0.4-1.3-0.4-2.2-0.4h-0.5v-2.4h0.5c1.3,0,2.8,0,3.8,1.2c0.8,0.9,1,2.4,0.8,4.5c-0.2,1.9-0.6,3.8-0.9,5.6c-0.4,2.1-0.9,4.1-0.8,5.7c0.2,3.6,1.5,5.5,4.1,5.5c2.8,0,4.3-1.9,4.3-5.7c0-3.1-0.9-5.6-1.5-7.2l-0.2-0.6c-0.6-1.8-0.7-3.4-0.8-5.7c-0.1-2.3,0-4.8,0.6-7.3c0.6-2.5,1.8-3.7,3.9-5.7l0.5-0.5l1.5,1.8l-0.5,0.5c-1.9,1.9-2.8,2.7-3.2,4.5c-0.5,2.2-0.5,4.5-0.5,6.6c0.1,2.3,0.1,3.6,0.6,5l0.2,0.6c0.6,1.7,1.7,4.5,1.7,8.1C-407.5,562.7-409.9,565.8-414,565.8L-414,565.8z M-411.1,544.4l-0.1-2.4c0,0,1.7,0,2.7-0.1c0-0.4,0-1.1,0-1.8v-0.4c0-2.9-0.4-3.3-0.9-3.7c-0.2-0.2-0.4-0.3-0.5-0.5l1.6-1.6c0.1,0.1,0.2,0.2,0.3,0.3c0.9,0.9,1.7,1.8,1.7,5.6v0.4c0,1.3,0,2.1-0.1,2.7c-0.2,1-0.8,1.5-1.7,1.5C-408.7,544.4-411.1,544.4-411.1,544.4L-411.1,544.4z"/><path id="Shape_1_" fill="#1B1B1C" d="M-433.7,451.5l0.2,66h24.3l3.6,17l18.5-17l58.4-0.2v-67.8h-105.1V451.5z"/></g><text transform="matrix(1 0 0 1 -422 500)" fill="#FFFFFF" font-family="ArialMT" font-size="9">12gal @ Chevron</text><image overflow="visible" enable-background="new    " width="2000" height="2235" id="Chevron-logo" xlink:href="1082CB53D4AC3DD1.png"  sketch:type="MSBitmapLayer" transform="matrix(1.300000e-02 0 0 1.297539e-02 -421 455)"></image><text transform="matrix(1 0 0 1 -385 478)" fill="#FFFFFF" font-family="ArialMT" font-size="24">$50</text></g></g></g></svg>';
 angular.module('benzpay', ['ngRoute'])
 	.controller('MainController', function($scope, $route, $routeParams){
 		$scope.activeTab;
@@ -42,11 +81,25 @@ angular.module('benzpay', ['ngRoute'])
 				// Zoom level for the map
 				zoomLevel: 17,
 				// Map center coordinates
-				center: [37.4121407222, -122.204705778],
+				center: [37.8085741667, -122.415423667],
 				baseMapType: nokia.maps.map.Display.SMARTMAP
 			}
 		);
 		map.addListener("displayready", function() {
+
+			var marker = new nokia.maps.map.StandardMarker([37.8085741667, -122.415423667], {
+				text: "Hi!" // Small label
+			});
+			var marker = new nokia.maps.map.Marker(
+				new nokia.maps.geo.Coordinate(37.8085741667, -122.415423667),{
+					title: 'Gas Station',
+					visibility: true,
+					icon: chevronMarker,
+					anchor: new nokia.maps.util.Point(32, 32)
+				}
+			);
+			map.objects.add(marker);
+
 			// Place code implementing the functionality of app
 			// here, for example to manipulate the map view.
 			window.WebSocket = window.WebSocket || window.MozWebSocket;
@@ -64,8 +117,8 @@ angular.module('benzpay', ['ngRoute'])
 					console.log('command', vehicleData.command);
 				}
 				else {
-					console.log(vehicleData);
-					map.setCenter([vehicleData.GPS_Latitude, vehicleData.GPS_Longitude]);
+					//console.log(vehicleData);
+					//map.setCenter([vehicleData.GPS_Latitude, vehicleData.GPS_Longitude]);
 				}
 			};
 		});
--- webapp/server.js
+++ webapp/server.js
@@ -3,14 +3,25 @@ var app = express();
 var request = require('request');
 var WebSocketServer = require('ws').Server;
 var events = require("events");
-var EventEmitter = require("events").EventEmitter;
-var ee = new EventEmitter();
-var serialport = require( "serialport" );
-var SerialPort = serialport.SerialPort;
 
-var vehicleUri = "http://172.31.99.4/vehicle";
+var vehicleUri = "http://172.31.99.3/vehicle";
 var vehicleData = null;
 var vehicleDataInterval = null;
+var serialport = require( "serialport" );
+
+var glob = require( "glob" )
+
+var serialPortFilenames = ( glob.sync( "/dev/tty.usbserial-*" ) ||
+glob.sync( "/dev/ttyUSB*") )
+if ( ! serialPortFilenames ) {
+	console.log( "Serial port not found" )
+	process.exit( 1 )
+} else if ( serialPortFilenames.length > 2 ) {
+	console.log( "You have more than one USB serial port connected!" )
+	process.exit( 1 )
+}
+var serialPortFilename = serialPortFilenames[ 0 ]
+var serialPortFilename = "/dev/tty.usbserial-FTGQJ7IM"
 
 app.use('/public', express.static('public'));
 
@@ -50,20 +61,100 @@ wss.on('connection', function connection(ws) {
 	});
 
 	//Send controls
-	var sp = new SerialPort("/dev/tty.usbserial-FTGQJ7IM", {
-		parser: serialport.parsers.readline( "\n" ),
-		baudrate: 115200,
-		databits: 8
-	});
-	sp.on('open', function () {
-		console.log('command connect opened.');
-		sp.on('data', function ( data ) {
-			console.log('command message', {'command': data} );
-			var command = {'command': data};
+	try {
+		var SerialPort = serialport.SerialPort;
 
-			ws.send(JSON.stringify(command));
+		var sp = new SerialPort("/dev/tty.usbserial-FTGQJ7IM", {
+			parser: serialport.parsers.readline("\n"),
+			baudrate: 115200,
+			databits: 8
 		});
-	});
+		sp.on('error', function(){
+			console.log('comand error');
+		});
+		sp.on('open', function () {
+			console.log('command connect opened.');
+			sp.on('data', function ( data ) {
+				splitData = data.split( " " )
+				eventName = splitData[ 0 ]
+				inArgs = {}
+				for ( arg in splitData.slice( 1)[0] )  {
+					splitArg = arg.split( ":" )
+					if(typeof splitArg[1] !== 'undefined') {
+						inArgs[splitArg[0].trim()] = splitArg[1].trim()
+					}
+				}
+
+				command = null
+				arg = null
+				switch ( eventName.trim() ) {
+					case 'touchRotate':
+					case 'cceRotate':
+						var change = splitData.slice(1)[0].split(':')[1];
+						//change = inArgs[ 'change' ]
+						if ( change < 0 ) {
+							command = 'ccw'
+							arg = 0 - change
+						} else {
+							command = 'cw'
+							arg = change
+						}
+						break;
+					case 'swipeUp':
+						// this often = 2 when only one touch occurred
+						arg = inArgs[ 'touches' ]
+					case 'pushUpPressed':
+						command = 'up'
+						break;
+					case 'swipeDown':
+						// this often = 2 when only one touch occurred
+						arg = inArgs[ 'touches' ]
+					case 'pushDownPressed':
+						command = 'down'
+						break;
+					case 'swipeLeft':
+						// this often = 2 when only one touch occurred
+						arg = inArgs[ 'touches' ]
+					case 'pushLeftPressed':
+						command = 'left'
+						break;
+					case 'swipeRight':
+						// this often = 2 when only one touch occurred
+						arg = inArgs[ 'touches' ]
+					case 'pushRightPressed':
+						command = 'right'
+						break;
+					case 'touchpadAreaPressed':
+					case 'selectPressed':
+						command = 'click'
+						break;
+					case 'touchpadBackPressed':
+					case 'cceBackPressed':
+						command = 'back'
+						break;
+					case 'touchpadFavoritePressed':
+					case 'cceFavoritePressed':
+						command = 'favorite'
+						break;
+					case 'touchpadMediaPressed':
+						command = 'menu'
+						break;
+				}
+				if(command) {
+					var commandWithArg = {
+						'command': {
+							'command': command,
+							'arg': arg
+						}
+					};
+					console.log('command message', commandWithArg);
+					ws.send(JSON.stringify(commandWithArg));
+				}
+			});
+		});
+	} catch(e){
+		console.log('comand error', e);
+	}
 
 	//Send vehicleData
 	vehicleDataInterval = setInterval(function(){
