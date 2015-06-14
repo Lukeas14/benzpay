7ac3362a6e2a0cf04f62dec3a35a170088a1e7e6
Justin Lucas <Lukeas14@gmail.com>
2015-06-14 10:57:58 -0700

    got command working

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -40,8 +40,9 @@ var chevronMarker = '<svg version="1.1" id="Layer_1" xmlns:sketch="http://www.bo
 
 var chevronMarker = '<svg version="1.1" id="Layer_1" xmlns:sketch="http://www.bohemiancoding.com/sketch/ns" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="-440 443 118 123" enable-background="new -440 443 118 123" xml:space="preserve"> <title>Chevron Marker</title> <desc>Created with Sketch.</desc> <g id="Page-1" sketch:type="MSPage"><g id="Map-menu" transform="translate(-496.000000, -384.000000)" sketch:type="MSArtboardGroup"><g id="Chevron-Marker" transform="translate(496.000000, 384.000000)" sketch:type="MSLayerGroup"><g id="Fuel-Marker-green" sketch:type="MSShapeGroup"><g id="_x37_7_Essential_Icons_11_"><path id="Message" fill="#2BAA5E" d="M-325.8,443.2h-110.9c-1.8,0-3.3,1.5-3.3,3.3v74c0,1.8,1.5,3.3,3.3,3.3h24.3c0.1,0,0.2,0.1,0.2,0.2v18.4c0,2.8,3.2,4.2,5.2,2.2l21-20.7c0,0,0.1-0.1,0.1-0.1h60c1.8,0,3.3-1.5,3.3-3.3v-74C-322.5,444.6-324,443.2-325.8,443.2L-325.8,443.2z M-328.6,517.4c0,0.1-0.1,0.2-0.2,0.2h-58.2c-0.8,0-1.5,0.3-2.2,0.9l-16.4,16.1c-0.1,0.1-0.3,0-0.3-0.1v-13.6c0-1.8-1.5-3.3-3.3-3.3h-24.3c-0.1,0-0.2-0.1-0.2-0.2v-65.8c0-0.1,0.1-0.2,0.2-0.2l104.7-2c0.1,0,0.2,0.1,0.2,0.2V517.4L-328.6,517.4z"/></g><path id="Shape" fill="#2BAA5E" d="M-422.9,527.8h-15.3c-0.9,0-1.6,0.8-1.6,1.7v35.5h18.6v-35.5C-421.3,528.6-422.1,527.8-422.9,527.8L-422.9,527.8z M-423.6,541.1h-13.9v-9.9h13.9V541.1L-423.6,541.1z M-414,565.8c-3.8,0-6.1-2.8-6.3-7.8c-0.1-1.9,0.4-4.1,0.8-6.4c0.4-1.7,0.7-3.5,0.9-5.3c0.2-1.8-0.1-2.4-0.3-2.7c-0.3-0.4-1.3-0.4-2.2-0.4h-0.5v-2.4h0.5c1.3,0,2.8,0,3.8,1.2c0.8,0.9,1,2.4,0.8,4.5c-0.2,1.9-0.6,3.8-0.9,5.6c-0.4,2.1-0.9,4.1-0.8,5.7c0.2,3.6,1.5,5.5,4.1,5.5c2.8,0,4.3-1.9,4.3-5.7c0-3.1-0.9-5.6-1.5-7.2l-0.2-0.6c-0.6-1.8-0.7-3.4-0.8-5.7c-0.1-2.3,0-4.8,0.6-7.3c0.6-2.5,1.8-3.7,3.9-5.7l0.5-0.5l1.5,1.8l-0.5,0.5c-1.9,1.9-2.8,2.7-3.2,4.5c-0.5,2.2-0.5,4.5-0.5,6.6c0.1,2.3,0.1,3.6,0.6,5l0.2,0.6c0.6,1.7,1.7,4.5,1.7,8.1C-407.5,562.7-409.9,565.8-414,565.8L-414,565.8z M-411.1,544.4l-0.1-2.4c0,0,1.7,0,2.7-0.1c0-0.4,0-1.1,0-1.8v-0.4c0-2.9-0.4-3.3-0.9-3.7c-0.2-0.2-0.4-0.3-0.5-0.5l1.6-1.6c0.1,0.1,0.2,0.2,0.3,0.3c0.9,0.9,1.7,1.8,1.7,5.6v0.4c0,1.3,0,2.1-0.1,2.7c-0.2,1-0.8,1.5-1.7,1.5C-408.7,544.4-411.1,544.4-411.1,544.4L-411.1,544.4z"/><path id="Shape_1_" fill="#1B1B1C" d="M-433.7,451.5l0.2,66h24.3l3.6,17l18.5-17l58.4-0.2v-67.8h-105.1V451.5z"/></g><text transform="matrix(1 0 0 1 -422 500)" fill="#FFFFFF" font-family="ArialMT" font-size="9">12gal @ Chevron</text><image overflow="visible" enable-background="new    " width="2000" height="2235" id="Chevron-logo" xlink:href="1082CB53D4AC3DD1.png"  sketch:type="MSBitmapLayer" transform="matrix(1.300000e-02 0 0 1.297539e-02 -421 455)"></image><text transform="matrix(1 0 0 1 -385 478)" fill="#FFFFFF" font-family="ArialMT" font-size="24">$50</text></g></g></g></svg>';
 angular.module('benzpay', ['ngRoute'])
-	.controller('MainController', function($scope, $route, $routeParams){
+	.controller('MainController', function($scope, $route, $location, $routeParams){
 		$scope.activeTab;
+		$scope.active = true;
 		$scope.$route = $route;
 		console.log('main controller');
 
@@ -56,99 +57,101 @@ angular.module('benzpay', ['ngRoute'])
 				console.log('websocket error', error);
 			};
 			connection.onmessage = function (message) {
-				var vehicleData = JSON.parse(message.data);
-
-				if(vehicleData.command){
-					console.log('command', vehicleData.command);
+				try{
+					var command = JSON.parse(message.data).command;
+				} catch(e){
+					console.log('bad command json');
 				}
 
-				if(vehicleData.command==="right"){
+				console.log('command', command.command, $location.url());
 
-					//get current state
-					if($state.$current==="home"){
-						$state.go('wifi')
 
+				if(command.command==="right"){
+
+					switch($location.url()){
+						case "/home":
+							$location.url('/wifi');
+							$scope.$apply();
+							break;
+						case "/wifi":
+							$location.url('/warnings');
+							$scope.$apply();
+							break;
+						case "/warnings":
+							$location.url('/peers');
+							$scope.$apply();
+							break;
+						case "/peers":
+							$location.url("/settings");
+							$scope.$apply();
+							break;
+						case "/settings":
+							$location.url("/pay");
+							$scope.$apply();
+							break;
+						case "/pay":
+							$location.url("/parking");
+							$scope.$apply();
+							break;
+						case "/parking":
+							$location.url("/drive");
+							$scope.$apply();
+							break;
+						case "/drive":
+							$location.url("/home");
+							$scope.$apply();
+							break;
 					}
-					if($state.$current==="wifi"){
-						$state.go('warnings')
-
-					}
-					if($state.$current==="warnings"){
-						$state.go('peers')
-
-					}
-					if($state.$current==="peers"){
-						$state.go('settings')
-
-					}
-
-					if($state.$current==="settings"){
-						$state.go('pay')
-
-					}
-
-					if($state.$current==="pay"){
-						$state.go('parking')
-
-					}
-					if($state.$current==="parking"){
-						$state.go('drive')
-
-					}
-					if($state.$current==="drive"){
-						$state.go('home')
-
-					}
-
 				}				
 
-				if(vehicleData.command==="left"){
+				if(command.command==="left"){
+					switch($location.url()){
 
-					//get current state
-					if($state.$current==="home"){
-						$state.go('drive')
-
-					}
-					if($state.$current==="drive"){
-						$state.go('parking')
-
-					}
-					if($state.$current==="parking"){
-						$state.go('pay')
-
-					}
-					if($state.$current==="pay"){
-						$state.go('settings')
-
-					}
-
-					if($state.$current==="settings"){
-						$state.go('peers')
-
-					}
-
-					if($state.$current==="peers"){
-						$state.go('warnings')
-
-					}
-					if($state.$current==="warnings"){
-						$state.go('wifi')
-
-					}
-					if($state.$current==="wifi"){
-						$state.go('home')
-
+						case "/home":
+							$location.url("/drive");
+							$scope.$apply();
+							break;
+						case "/drive":
+							$location.url("/parking");
+							$scope.$apply();
+							break;
+						case "/parking":
+							$location.url("/pay");
+							$scope.$apply();
+							break;
+						case "/pay":
+							$location.url("/settings");
+							$scope.$apply();
+							break;
+						case "/settings":
+							$location.url("/peers");
+							$scope.$apply();
+							break;
+						case "/peers":
+							$location.url("/warnings");
+							$scope.$apply();
+							break;
+						case "/warnings":
+							$location.url("/wifi");
+							$scope.$apply();
+							break;
+						case "/wifi":
+							$location.url("/home");
+							$scope.$apply();
+							break;
 					}
 
 				}
 
-				if(vehicleData.command==="menu"){
+				if(command.command==="menu"){
 
-					$state.go('home')
+					$location.url('/pay')
+					$scope.$apply();
 				}
-				if(vehicleData.command==="back"){
+				if(command.command==="back"){
 
-					$state.go('home')
+					$location.url('/pay')
+					$scope.$apply();
 				}
 				else {
 					//console.log(vehicleData);
@@ -217,7 +220,8 @@ angular.module('benzpay', ['ngRoute'])
 				}
 			);
 			map.objects.add(marker);
-*/
+			*/
+
 			// // Place code implementing the functionality of app
 			// // here, for example to manipulate the map view.
 			// window.WebSocket = window.WebSocket || window.MozWebSocket;
@@ -241,11 +245,11 @@ angular.module('benzpay', ['ngRoute'])
 			};
 			connection.onmessage = function (message) {
 				var vehicleData = JSON.parse(message.data);
-				console.log(vehicleData.GPS_Latitude, vehicleData.GPS_Longitude);
+				//console.log(vehicleData.GPS_Latitude, vehicleData.GPS_Longitude);
 
 				if(!markersSet) {
 					for (var i = 0; i < vehicleData.points.length; i++) {
-						console.log('OOINT', vehicleData.points[i]);
+						//console.log('OOINT', vehicleData.points[i]);
 						var marker = new nokia.maps.map.Marker(
 							new nokia.maps.geo.Coordinate(vehicleData.points[i].latitude, vehicleData.points[i].longitude), {
 								title: vehicleData.points[i].name,
@@ -265,11 +269,51 @@ angular.module('benzpay', ['ngRoute'])
 				map.setCenter([vehicleData.GPS_Latitude, vehicleData.GPS_Longitude]);
 			};
 		});
-		//$scope.points = [{'name':'wtf2323'}];
-
 	})
 
 	.controller('ParkingController', function($scope, $routeParams) {
+		$scope.payments = [
+			{
+				name: "Redwood Chevron",
+				amount: "$53.21",
+				account: "1CsyGSz42Adi3..."
+			},
+			{
+				name: "Starbucks Tall Coffee",
+				amount: "$5.95",
+				account: "Visa ending in 9852"
+			},
+			{
+				name: "Bridge Toll Pass",
+				amount: "$6.00",
+				account: "Visa ending in 9852"
+			},
+			{
+				name: "Palo Alto Shell",
+				amount: "$25.71",
+				account: "1CsyGSz42Adi3..."
+			},
+			{
+				name: "Redwood Chevron",
+				amount: "$53.21",
+				account: "Visa ending in 9852"
+			},
+			{
+				name: "Starbucks Tall Coffee",
+				amount: "$5.95",
+				account: "1CsyGSz42Adi3..."
+			},
+			{
+				name: "Bridge Toll Pass",
+				amount: "$6.00",
+				account: "Visa ending in 9852"
+			},
+			{
+				name: "Palo Alto Shell",
+				amount: "$25.71",
+				account: "1CsyGSz42Adi3..."
+			},
+		]
 	})
 
 	.controller('DriveController', function($scope, $routeParams) {
@@ -310,7 +354,7 @@ angular.module('benzpay', ['ngRoute'])
 				controller: 'DriveController'
 			})
 			.otherwise({
-				redirectTo: '/home'
+				redirectTo: '/pay'
 			});
 
 		// configure html5 to get links working on jsfiddle
